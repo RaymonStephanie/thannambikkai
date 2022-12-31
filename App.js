7e09836143c652b97ef13605d4495536fadcbdaf
lulzsimp <natechnologies2007@gmail.com>
2022-12-31 10:44:45 +0530

    licensing

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import App from './src/Index';
-import * as React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import App from "./src/Index"
+import * as React from "react"
 
 export default function Main() {
   return (
     <PaperProvider>
       <App />
     </PaperProvider>
-  );
+  )
 }
